2391959d76bb6ac02001f0800a67f2d6380c302c
shuvo339 <shubhroshafiullah@gmail.com>
2024-02-12 23:54:13 +0600

    set common function

--- parallelogram.js
+++ parallelogram.js
@@ -5,6 +5,20 @@ function calculatedParallelogramArea(){
     setTextArea('parallelogram-area', area);
 }
 
+function calculatedRhombusArea(){
+    const base = getInputValueById('rhombus-base');
+    const height = getInputValueById('rhombus-height');
+    const area = 0.5 * base * height;
+    setTextArea('rhombus-area', area);
+}
+
+function calculatedPentagonArea(){
+    const base = getInputValueById('pentagon-base');
+    const height = getInputValueById('pentagon-height');
+    const area = 0.5 * base * height;
+    setTextArea('pentagon-area', area);
+}
+
 function getInputValueById(inputFieldId){
     const inputField = document.getElementById(inputFieldId);
     const inputValueText = inputField.value;
